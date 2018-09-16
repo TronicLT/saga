137c6a3950e31e4c6f0b0c88c70b0c9c06e6b601
Todani Luvhengo <todani.uml@gmail.com>
2018-09-16 22:09:40 +0200

    Added Keras style Callbacks conatainer to utils/callbacks.py

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -1,14 +1,100 @@
 # coding=utf8
 from collections import OrderedDict
 from torch.nn import Module
+from datetime import datetime
 
 
 __author__ = 'Todani Luvhengo'
 __email__ = 'todani.uml@gmail.com'
 
 __all__ = [
-    'Callback'
+    'Callback',
+    'Callbacks'
 ]
+
+
+def _get_current_time():
+    return datetime.now().strftime("%B %d, %Y - %I:%M%p")
+
+
+class Callbacks(object):
+    """ Container holding a list of callbacks.
+
+    Parameters
+    ----------
+    callbacks : iterable
+        List of call back objects
+
+    """
+    def __init__(self, callbacks=None, queue_length=10):
+        self.model_ = None
+        callbacks = callbacks or []
+        self.callbacks = [c for c in callbacks]
+        self.queue_length = queue_length
+
+    def append(self, callback):
+        self.callbacks.append(callback)
+
+    @property
+    def params(self):
+        return [callback.params for callback in self.callbacks]
+
+    @params.setter
+    def params(self, params):
+        for callback in self.callbacks:
+            callback.set_params(params)
+
+    @property
+    def model(self):
+        return self.model_
+
+    @model.setter
+    def model(self, model):
+        assert isinstance(model, Module), '`model` argument must inherit from torch.nn.Module'
+        self.model_ = model
+        for callback in self.callbacks:
+            callback.set_model(model)
+
+    def set_params(self, params):
+        self.params = params
+
+    def set_model(self, model):
+        self.model = model
+
+    def on_epoch_begin(self, epoch, logs=None):
+        logs = logs or dict()
+        for callback in self.callbacks:
+            callback.on_epoch_begin(epoch, logs)
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or dict()
+        for callback in self.callbacks:
+            callback.on_epoch_end(epoch, logs)
+
+    def on_batch_begin(self, batch, logs=None):
+        logs = logs or dict()
+        for callback in self.callbacks:
+            callback.on_batch_begin(batch, logs)
+
+    def on_batch_end(self, batch, logs=None):
+        logs = logs or dict()
+        for callback in self.callbacks:
+            callback.on_batch_end(batch, logs)
+
+    def on_train_begin(self, logs=None):
+        logs = logs or dict()
+        logs['start_time'] = _get_current_time()
+        for callback in self.callbacks:
+            callback.on_train_begin(logs)
+
+    def on_train_end(self, logs=None):
+        logs = logs or dict()
+        for callback in self.callbacks:
+            callback.on_train_end(logs)
+        logs['stop_time'] = _get_current_time()
+
+    def __iter__(self):
+        return iter(self.callbacks)
 
 
 class Callback(object):
@@ -69,3 +155,7 @@ class Callback(object):
 
     def on_train_end(self, logs=None):
         pass
+
+
+class History(Callback):
+    pass
