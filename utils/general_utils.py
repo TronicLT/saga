7ec58ba68dcc5a38f9cb8cfc06c28a1e3c8f8021
Todani Luvhengo <todani.uml@gmail.com>
2018-09-18 20:10:51 +0200

    Added check_attribute function to general_utils.py module

--- utils/general_utils.py
+++ utils/general_utils.py
@@ -6,7 +6,8 @@ __author__ = 'Todani Luvhengo'
 __email__ = 'todani.uml@gmail.com'
 
 __all__ = [
-    'remove_duplicates'
+    'remove_duplicates',
+    'check_attribute'
 ]
 
 
@@ -27,3 +28,44 @@ def remove_duplicates(obj):
         if a is b:
             obj.remove(a)
     return obj
+
+
+def check_attribute(obj, attributes, msg=None, all_or_any=all):
+    """Perform validation for an attribute(s) and raise error if doesnt exist.
+
+    based on
+
+    Parameters
+    ----------
+    obj : object.
+        estimator instance for which the check is performed.
+
+    attributes : str or iterable
+        attribute name(s) given as string or a list/tuple of strings
+
+    msg : string
+        The default error message is, "This %(name)s does not contain %(attribute)s"
+
+        For custom messages if "%(name)s" is present in the message string,
+        it is substituted for the estimator name.
+
+    all_or_any : callable, {all, any}, default all
+        Specify whether all or any of the given attributes must exist.
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    AttributeError
+        If the attributes are not found.
+    """
+    if msg is None:
+        msg = "This %(name)s instance does not have %(attribute)s"
+
+    if not isinstance(attributes, (list, tuple)):
+        attributes = [attributes]
+
+    if not all_or_any([hasattr(obj, attr) for attr in attributes]):
+        raise AttributeError(msg % {'name': type(obj).__name__, 'attribute': attributes})
